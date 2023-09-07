72b1adcf7d92f88594dac8aecc62bc24f63f4081
RL08 <118910391+RL08@users.noreply.github.com>
2023-09-07 19:37:46 +0200

    Create startServer.cmd

---
+++ startServer.cmd
@@ -0,0 +1,11 @@
+@echo off
+docker rm -f chathub_sqlserver 2> nul
+docker run -d -p 11433:1433 --name chathub_sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SqlServer2019" mcr.microsoft.com/azure-sql-edge
+IF ERRORLEVEL 1 GOTO error
+dotnet build ChatHubProject\ChatHubProject.Webapi --no-incremental --force
+dotnet watch run -c Debug --project ChatHubProject\ChatHubProject.Webapi
+GOTO end
+
+:error
+PAUSE
+:end
