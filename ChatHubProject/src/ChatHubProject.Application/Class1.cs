769b80de3b8faaaafb503c7ed92bfcc1554c3253
richard <liu22291@spengergasse.at>
2023-09-10 19:50:19 +0200

    new project structure

---
+++ ChatHubProject/src/ChatHubProject.Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ChatHubProject.Application;
+public class Class1
+{
+
+}
---
+++ ChatHubProject/test/ChatHubProject.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ChatHubProject.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
