2f79413ad2a1c0d461c3c420ea14f16d222e373a
Mohamad Aldulemi <ALU22162@spengergasse.at>
2023-12-07 15:07:15 +0100

    delete_duplicate

---
+++ ChatHubProject/src/ChatHubProject.Client/src/components/ChatRoom.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>
+      <!-- Eingabefeld für die Nachricht -->
+      <input
+        v-model="message"
+        @keyup.enter="SendMessage"
+        placeholder="Send Message to"
+      />
+
+      <!-- Nachrichten-Liste -->
+      <ul>
+        <li v-for="nachricht in nachrichten" :key="nachricht.id">
+          {{ nachricht.text }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+<style>
+input{
+width: 50%;
+background: var(--theme-middle);
+color: white;
+}
+
+
+</style>
