66f55e92b5b4ed608d0c39c1f2ed65a4f347bbca
Mohamad Aldulemi <ALU22162@spengergasse.at>
2023-12-07 13:44:07 +0100

    Cookies

--- ChatHubProject/src/ChatHubProject.Client/src/main.js
+++ ChatHubProject/src/ChatHubProject.Client/src/main.js
@@ -13,10 +13,17 @@ import { faArrowLeftLong, faArrowRightLong, faGear } from '@fortawesome/free-sol
 library.add(faArrowLeftLong, faArrowRightLong, faGear)
 
 axios.defaults.baseURL = process.env.NODE_ENV == 'production' ? "/api" : "https://localhost:7081/api";
-
+axios.defaults.withCredentials = true;
 const app = createApp(App)
 
+axios
+  .get("user/userinfo")
+  .then((response) => {
+    store.commit("authenticate", response.data);
+  })
+  .finally(() => {
 app.use(router)
 app.use(store)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
+  })
